b5d31e19f056e600424cd5fc78862d6f06678ea7
developersoftwarebrazil <contatodevelopersbrazildna@gmail.com>
2023-08-20 12:38:20 -0300

    create folders on infra.data's layaer create a dbcontext and create an initial migration and create an onitial data

---
+++ Financeiro-FULLSTACK/src/Backend/Backend.Financeiro.Domain/Entities/Identities/Users/ApplicationUser.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Backend.Financeiro.Domain.Entities.Identities.Users
+{
+    public class ApplicationUser : IdentityUser
+    {
+    }
+}
---
+++ Financeiro-FULLSTACK/src/Backend/Backend.Financeiro.Infra.Data/Context/TransactiondbContext.cs
@@ -0,0 +1,37 @@
+﻿using Backend.Financeiro.Domain.Entities.Categories;
+using Backend.Financeiro.Domain.Entities.Identities.Users;
+using Backend.Financeiro.Domain.Entities.Systems;
+using Backend.Financeiro.Domain.Entities.Systems.Transactions;
+using Backend.Financeiro.Domain.Entities.Systems.Users;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Backend.Financeiro.Infra.Data.Context
+{
+    public class TransactiondbContext : IdentityDbContext<ApplicationUser>
+    {
+        public TransactiondbContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<ApplicationUser> ApplicationUser { set; get; }
+        public DbSet<Category> Categories { set; get; }
+        public DbSet<FinanceSystem> FinanceSystem { set; get; }
+        public DbSet<FinanceSystemUser> FinanceSystemUsers { set; get; }
+        public DbSet<Transaction> Transactions { set; get; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(GetConnetioString());
+            base.OnConfiguring(optionsBuilder);
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<ApplicationUser>().ToTable("AspNetUsers").HasKey(t => t.Id);
+            base.OnModelCreating(builder);
+        }
+        public string GetConnetioString()
+        {
+            return "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Finance_Fullstack_2023;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+        }
+
+    }
+}
